0ab208a7edb8b0b2ae78dee615dbffade16a18e6
seoyun-0626 <rsy2169@gmail.com>
2025-12-10 04:56:49 +0900

    fix alias and disable turbopack for build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
-const path = require("path");
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // alias 설정
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname, 'app');
     config.resolve.alias['@features'] = path.resolve(__dirname, 'features');
     return config;
-  }
-}
+  },
+  // Turbopack 비활성화
+  turbopack: {},
+};
 
 module.exports = nextConfig;
