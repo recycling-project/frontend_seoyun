1f6f074e17d4aed6afb3edf92babd20e440c29dc
seoyun-0626 <rsy2169@gmail.com>
2025-12-10 05:35:39 +0900

    경로수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 //layout.tsx — “모든 페이지를 감싸는 최상위 레이아웃”
 
 import "../styles/globals.css";
-import KioskScaler from "@/components/ui/KioskScaler";
+import KioskScaler from "@components/ui/KioskScaler";
 
 export const metadata = {
   title: "순환마루 키오스크",
